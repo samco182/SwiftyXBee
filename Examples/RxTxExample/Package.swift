3b345b1869a640a5bdcbc715ce86c896cd0486ee
Samuel Cornejo <sam.ach1990@gmail.com>
2019-09-13 09:31:15 -0600

    [Example] moves RxTx example to its own folder

--- Examples/RxTxExample/Package.swift
+++ Examples/RxTxExample/Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyXBeeExample",
+    name: "RxTxExample",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/samco182/SwiftyXBee", from: "1.0.0"),
-        
+        //.package(url: "https://github.com/samco182/SwiftyXBee", from: "1.0.0"),
+        .package(path: "../../"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SwiftyXBeeExample",
+            name: "RxTxExample",
             dependencies: ["SwiftyXBee"]),
         .testTarget(
-            name: "SwiftyXBeeExampleTests",
-            dependencies: ["SwiftyXBeeExample"]),
+            name: "RxTxExampleTests",
+            dependencies: ["RxTxExample"]),
     ]
 )
--- Examples/RxTxExample/Sources/RxTxExample/main.swift
+++ Examples/RxTxExample/Sources/RxTxExample/main.swift
@@ -4,9 +4,7 @@ import SwiftyXBee
 let serialConnection = SerialConnection(speed: .S9600, bitsPerChar: .Eight, stopBits: .One, parity: .None)
 let xbee = SwiftyXBee(for: .RaspberryPi3, serialConnection: serialConnection)
 
-
 // Reading a Receive Packet API Frame
-
 print("Receiving packet...")
 
 do {
@@ -16,17 +14,14 @@ do {
     print("Error receiving packet: \(error)")
 }
 
-
 // Sending a Transmit Request API Frame
-
 print("Sending packet...")
+
 let deviceAddress = DeviceAddress(address: 0x0013A20012345678) // For the example to work, replace this for an actual XBee Serial number
 let networkAddress = NetworkAddress(address: 0xFFFE) // You can use this address if you don't know the destination's network address
 xbee.sendTransmitRequest(to: deviceAddress, network: networkAddress, message: "This is my message to send!")
 
-
 // Reading a Transmit Status API Frame
-
 do {
     let readingPacket = try xbee.readTransmitStatus()
     print("Packet sent. Status: \(readingPacket.frameData.deliveryStatus!)")
