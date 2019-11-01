de2612d5e68dc0b87c6f3d12ab28e5059c75aa2d
Samuel Cornejo <sam.ach1990@gmail.com>
2019-11-01 12:56:17 -0600

    [Lib] updates SwiftyGPIO library to use an updated branch

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.0.0"),
+        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", .exact("2.0.0-beta10")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
